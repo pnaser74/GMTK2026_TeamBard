d019ec4f3499b215f8cbe51b6311bbccf4f956b7
pnaser74 <p.naser74@gmail.com>
2026-07-22 18:56:45 -0700

    add more FMOD events

--- Assets/Scripts/Audio/AudioDataSO.cs
+++ Assets/Scripts/Audio/AudioDataSO.cs
@@ -11,9 +11,26 @@ public class AudioDataSO : ScriptableObject
     public FMODUnity.EventReference titleMusicEvent;
     public FMODUnity.EventReference gameMusicEvent;
 
-    [Header ("SFX Event References")]
+    [Header ("Player SFX Event References")]
     public FMODUnity.EventReference footstepsEvent;
     public FMODUnity.EventReference jumpEvent;
+    public FMODUnity.EventReference takeDamageEvent;//in essence this is just losing the Count
+    
+    [Header ("Count SFX Event References")]
+    public FMODUnity.EventReference wingsFlappingEvent;
+    // public FMODUnity.EventReference jumpEvent;
+    
+    [Header ("Enemy SFX Event References")]
+    public FMODUnity.EventReference torchAttackEvent;
+    public FMODUnity.EventReference pitchforkAttackEvent;
+    
+    [Header ("Environmental SFX Event References")]
+    public FMODUnity.EventReference platformCollapseEvent;
+    public FMODUnity.EventReference burningObjectEvent;
+
+    [Header ("Voice Lines Event References")]
+    public FMODUnity.EventReference countDialogueEvent;
+    public FMODUnity.EventReference villagerDialogueEvent;
 
      [Header("FMOD UI Event References")]
     public FMODUnity.EventReference buttonClickEvent;
